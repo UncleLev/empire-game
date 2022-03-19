20f221e648ba4eaa7c5a193ff7d0d7635f200f43
UncleLev <zakhar.levchuk@softermii.com>
2022-03-19 18:55:46 +0200

    add copy of store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,18 @@
+import { throttle } from 'lodash';
 import { createStore, combineReducers } from 'redux';
+import { loadState, saveState } from 'src/service/helpers/store.helpers';
 import gameReducer from './game/game.reducer';
 
+const persistedState = loadState();
 const reducer = combineReducers({ game: gameReducer });
 
-const store = createStore(reducer);
+const store = createStore(reducer, persistedState);
+
+store.subscribe(
+    throttle(() => {
+        saveState(store.getState());
+    }, 1000),
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 
